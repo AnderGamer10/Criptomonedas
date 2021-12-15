dc166996d92be1dc731e4a241d423900911afc04
AnderGamer10 <anderedondo10@hotmail.com>
2021-12-15 15:31:34 +0100

    Agregar archivos de proyecto.

---
+++ Criptomonedas/Models/CriptoItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Criptomonedas.Models
+{
+    public class CriptoItems
+    {
+        public string Nombre { get; set; }
+        public double ValorMaximo { get; set; }
+        public double ValorActual { get; set; } 
+    }
+}
